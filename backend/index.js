a431afc55c04d65e684f815577bcbf4108391073
Himanshu <hs7772043@gmail.com>
2024-01-07 23:00:18 +0530

    created sign up api and database to store user information

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,33 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import authRouter from './routes/auth.route.js'
 
 // dotenv configuration
 dotenv.config()
 
 const app = express()
 
+app.use(express.json())
+
 // db connection
 mongoose.connect(process.env.MONGO)
     .then(() => console.log('Database connected successfully!'))
     .catch((error) => console.log(error))
 
+app.use('/backend/auth', authRouter)
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server listening at PORT:${PORT}`)
+})
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
 })
