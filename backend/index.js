2fc3ec28a617d44e3b1b4aa97a9eaa0501fc7f4d
Himanshu <hs7772043@gmail.com>
2024-01-07 22:25:12 +0530

    created database connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+// dotenv configuration
+dotenv.config()
 
 const app = express()
+
+// db connection
+mongoose.connect(process.env.MONGO)
+    .then(() => console.log('Database connected successfully!'))
+    .catch((error) => console.log(error))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
