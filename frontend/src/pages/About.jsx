0988146905de110f3bc5164eb71ed4baac84c88c
Himanshu <hs7772043@gmail.com>
2024-01-09 18:29:08 +0530

    created about and home page ui

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,63 @@
 import React from 'react'
+import styled from 'styled-components';
 
 export default function About() {
   return (
-    <div>About</div>
+    <Wrapper>
+      <div className="container">
+        <div className="single-page-heading">
+          <h1>About Us</h1>
+        </div>
+        <div className="single-page-con">
+          <p>
+            Welcome to KGSUrlShortener, where we believe in simplifying the web for everyone. Our mission is to provide a seamless URL shortening service that enhances online accessibility while prioritizing user privacy and maintaining the integrity of your links.
+          </p>
+
+          <h2>Who We Are</h2>
+          <p>At KGSUrlShortener, we are a team of dedicated professionals committed to revolutionizing web navigation. With years of expertise in web technology, we aim to streamline lengthy URLs into concise and manageable links without compromising their accuracy.</p>
+
+          <h2>Our Commitment to Privacy</h2>
+          <p>We understand the importance of safeguarding your data. Rest assured, we prioritize the privacy of your information. Every URL shortened through our platform is handled with strict confidentiality. We do not track, store, or share any personal data linked to the URLs you shorten. Your trust in our service is paramount, and we uphold the highest standards of data security.</p>
+
+          <h2>Ensuring Accuracy and Reliability</h2>
+          <p>Accuracy matters. Our robust system guarantees that the shortened URLs generated on our platform are precise, reliable, and always redirect users to the intended destination. Each link undergoes a rigorous verification process to ensure its correctness before being saved into our secure database.</p>
+
+          <h2>Secure Database Management</h2>
+          <p>The URLs you shorten are securely stored in our advanced database infrastructure. Our systems employ state-of-the-art security measures to protect your links from unauthorized access or data breaches. We regularly update and maintain our database to ensure its resilience against potential threats, assuring you a safe and trustworthy service.</p>
+
+          <h2>Join Us in Simplifying the Web</h2>
+          <p>Experience the convenience of concise and efficient links with KGSUrlShortener. Whether for personal or professional use, our URL shortening service is designed to make sharing and accessing links hassle-free. Join us in simplifying the web while entrusting your privacy and link accuracy to a reliable platform.</p>
+
+          <p>Thank you for choosing KGSUrlShortener—simplify your links, elevate your browsing experience!</p>
+
+        </div>
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+    width: 800px;
+    margin: 50px auto 50px;
+
+  .single-page-heading{
+    margin-bottom: 20px;
+    text-align: center;
+
+    h1{
+      font-size: ${({ theme }) => theme.fontSize.headingFontSize};
+    }
+  }
+
+  .single-page-con{
+    p{
+      font-size: ${({ theme }) => theme.fontSize.textFontSize};
+      margin-bottom: 20px;
+    }
+
+    h2{
+      font-size: ${({ theme }) => theme.fontSize.smallHeadingFontSize};
+      margin-bottom: 10px;
+    }
+  }
+`;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,176 @@
 import React from 'react'
+import styled from 'styled-components'
+import { IoMdCopy } from "react-icons/io";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <Wrapper>
+      <div className="container">
+        <div className="url-body">
+          <div className="url-form">
+            <form action="" className='flex flex-h-align'>
+              <input type="text" placeholder='https://siteexample.com' />
+              <input type="submit" value="Shorten" />
+            </form>
+          </div>
+          <div className="url-list">
+            <table>
+              <thead>
+                <tr>
+                  <th>Sr. No.</th>
+                  <th>Original Url</th>
+                  <th>Shorten Url</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1.</td>
+                  <td>example.com</td>
+                  <td>localhost:3000/kjahiu <span className='copyUrl'><IoMdCopy /></span></td>
+                  <td>
+                    <button type='button' className='dltBtn'>Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2.</td>
+                  <td>example.com</td>
+                  <td>localhost:3000/kjahiu <span className='copyUrl'><IoMdCopy /></span></td>
+                  <td>
+                    <button type='button' className='dltBtn'>Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>3.</td>
+                  <td>example.com</td>
+                  <td>localhost:3000/kjahiu <span className='copyUrl'><IoMdCopy /></span></td>
+                  <td>
+                    <button type='button' className='dltBtn'>Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>4.</td>
+                  <td>example.com</td>
+                  <td>localhost:3000/kjahiu <span className='copyUrl'><IoMdCopy /></span></td>
+                  <td>
+                    <button type='button' className='dltBtn'>Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>5.</td>
+                  <td>example.com</td>
+                  <td>localhost:3000/kjahiu <span className='copyUrl'><IoMdCopy /></span></td>
+                  <td>
+                    <button type='button' className='dltBtn'>Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  .url-body{
+    margin: 50px 0;
+    text-align: center;
+
+    .url-form{
+      width: 500px;
+      margin: 0 auto;
+
+      form{
+        flex-direction: column;
+        gap: 20px;
+      }
+      
+      input{
+        padding: 10px;
+        background-color: ${({ theme }) => theme.colors.quaternaryColor};
+        color: ${({ theme }) => theme.colors.primaryColor};
+        border: none;
+        outline: none;
+        font-size: ${({ theme }) => theme.fontSize.textFontSize};
+      }
+
+      input[type="submit"]{
+        background-color: ${({ theme }) => theme.colors.primaryColor};
+        color: ${({ theme }) => theme.colors.quaternaryColor};
+        font-size: ${({ theme }) => theme.fontSize.textFontSize};
+        border: none;
+        outline: none;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        transition: 0.25s;
+        cursor: pointer;
+
+        &:hover{
+          color: ${({ theme }) => theme.colors.primaryColor};
+          background-color: ${({ theme }) => theme.colors.quaternaryColor};
+        }
+      }
+
+    }
+
+    .url-list{
+      margin-top: 50px;
+      overflow-x: auto;
+
+      table {
+        width: 100%;
+        border-collapse: collapse;
+
+        th, td {
+          border: 1px solid ${({ theme }) => theme.colors.primaryColor};
+          padding: 10px;
+          position: relative;
+
+          &:first-child {
+            width: 10%;
+          }
+
+          &:nth-child(2) {
+            width: 60%;
+          }
+
+          &:nth-child(3) {
+            width: 20%;
+          }
+
+          &:nth-child(4) {
+            width: 10%;
+          }
+
+          button.dltBtn{
+            background-color: #f00;
+            padding: 5px 10px;
+            border: none;
+            outline: none;
+            border-radius: 0;
+            color: ${({ theme }) => theme.colors.quaternaryColor};
+            font-size: ${({ theme }) => theme.fontSize.textFontSize};
+            cursor: pointer;
+          }
+
+          .copyUrl{
+            position: absolute;
+            right: 0;
+            top: 0;
+            cursor: pointer;
+            height: 30px;
+            width: 30px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: ${({theme}) => theme.colors.tertiaryColor};
+            color: ${({theme}) => theme.colors.primaryColor};
+          }
+        }
+      }
+
+    }
+  }
+`;
