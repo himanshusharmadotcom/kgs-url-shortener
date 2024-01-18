0eb3593f546ce5fe35e07ffabd1318ee71de2375
Himanshu <hs7772043@gmail.com>
2024-01-18 19:39:53 +0530

    changed the base url

--- backend/controllers/service.controller.js
+++ backend/controllers/service.controller.js
@@ -16,9 +16,9 @@ function extractQueryParams(origUrl) {
 
 export const shortController = async (req, res, next) => {
     const { origUrl } = req.body
-    // const base = '/backend/service'
+    const base = 'https://kgs-url-shortener.onrender.com/backend/service'
 
-    const base = path.join(__dirname, 'backend/service');
+    // const base = path.join(__dirname, 'backend/service');
     if (validateUrl(origUrl)) {
         try {
             let url = await urlData.findOne({ origUrl })
